9623b03ebf91e4b458e52f4f132ed1c12bea0b60
PierrePineau <pierrepineau.pro@gmail.com>
2024-03-28 09:00:47 +0100

    Material

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import BottomNavigation from "./layout/BottomNavigation";
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <BottomNavigation />
     </div>
   );
 }
