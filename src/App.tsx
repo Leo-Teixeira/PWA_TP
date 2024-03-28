44cfd505b03a9283d283c6245a2504a179f17269
PierrePineau <pierrepineau.pro@gmail.com>
2024-03-28 15:29:30 +0100

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import "./App.css"
-import GeoLocalisation from "./components/GeoLocalisation/GeoLocalisation.tsx"
-import BottomNavigation from "./layout/BottomNavigation/BottomNavigation.tsx"
+import React from "react";
+import "./App.css";
+import BottomNavigation from "./layout/BottomNavigation/BottomNavigation.tsx";
+import { Route, Routes } from "react-router-dom";
+import RouteProvider from "./route-provider";
+import { TpRoute } from "./core/type/tpRoute";
 
 function App() {
   return (
