b8e9a72a5ba3614285e900c2cdfb4caa3dc00ba3
Leo-Teixeira <teixeiraleo72@gmail.com>
2024-03-29 08:44:15 +0100

    pushhhhhhhhhh

--- src/components/camera/camera.tsx
+++ src/components/camera/camera.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@mui/base";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Camera = () => {
   const [stream, setStream] = useState<MediaStream | null>(null);
+  const [camera, setCamera] = useState<boolean | null>(false);
+  const [buttonText, setButtonText] = useState<string | null>("Allumer caméra");
+  const [status, setStatus] = useState<boolean | null>(false);
+  const [photos, setPhotos] = useState<string | null>();
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    localStorage.setItem("listPhoto", JSON.stringify(photos));
+  }, [photos]);
 
   async function getStream() {
     let didCancel = false;
@@ -22,9 +32,7 @@ const Camera = () => {
         }
       }
     };
-
     getUserMedia();
-
     return () => {
       didCancel = true;
       if (stream) {
@@ -34,6 +42,13 @@ const Camera = () => {
   }
 
   async function getMedia() {
+    if (camera == false) {
+      setCamera(true);
+      setButtonText("Eteindre la caméra");
+    } else {
+      setCamera(false);
+      setButtonText("Allumer caméra");
+    }
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({
         audio: true,
@@ -46,17 +61,54 @@ const Camera = () => {
     }
   }
 
+  const takePhoto = () => {
+    // if (canvasRef.current && videoRef.current) {
+    //   const context = canvasRef.current.getContext("2d");
+    //   context?.drawImage(videoRef.current, 0, 0, 300, 200);
+    //   const data = canvasRef.current.toDataURL('"image/png"');
+    //   setPhotos((prevPhotos) => [...prevPhotos, data]);
+    const canvas = document.createElement("canvas");
+    canvas.width = 1920;
+    canvas.height = 1080;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(stream, 0, 0, canvas.width, canvas.height);
+
+    const url = canvas.toDataURL("image/jpeg");
+    setPhotos(url);
+    // canvasRef.current.toBlob((blob: any) => {
+    //   // const newImg = document.createElement("img");
+    //   const url = URL.createObjectURL(blob);
+
+    //   // newImg.onload = () => {
+    //   //   // no longer need to read the blob so it's revoked
+    //   //   URL.revokeObjectURL(url);
+    //   // };
+
+    //   // newImg.src = url;
+    //   // console.log(newImg.src);
+    //   alert(url);
+    //   setPhotos(url);
+    // });
+    // }
+  };
+
   return (
     <Box>
-      <Typography>Test</Typography>
-      <video
-        autoPlay
-        ref={(video) => {
-          if (video && stream) {
-            video.srcObject = stream;
-          }
-        }}></video>
-      <Button onClick={getMedia}>Allumer caméra</Button>
+      <Typography>Camera</Typography>
+      {camera && (
+        <Box>
+          <video
+            autoPlay
+            ref={(video) => {
+              if (video && stream) {
+                video.srcObject = stream;
+              }
+            }}></video>
+          <Button onClick={takePhoto}>Prendre une photo</Button>
+        </Box>
+      )}
+      <Button onClick={getMedia}>{buttonText}</Button>
+      <Box component="img" alt="image" src={photos}></Box>
     </Box>
   );
 };
