d7269a679e8e2fd840c04afe6a2a1bdbc2e73d99
PierrePineau <pierrepineau.pro@gmail.com>
2024-03-29 08:15:25 +0100

    Tp1

--- src/components/downloadPWA/downloadPWA.tsx
+++ src/components/downloadPWA/downloadPWA.tsx
@@ -1,13 +1,54 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import {Button} from "@mui/material"
 import { Box } from "@mui/system"
 import DownloadingIcon from '@mui/icons-material/Downloading';
 
 
-export default function downloadPWA() {
+export default function DownloadPWA() {
+    const [installable, setInstallable] = useState(false)
+    const [installPrompt, setInstallPrompt] = useState<any>(null)
+
+    useEffect(() => {
+        const handleBeforeInstallPrompt = (event : any) => {
+            // Gérer l'événement beforeinstallprompt ici
+            console.log('L\'événement beforeinstallprompt a été déclenché !');
+            //   event.preventDefault(); // Empêche le navigateur d'afficher la fenêtre d'installation par défaut
+            setInstallPrompt(event)
+            setInstallable(true)
+        };
+    
+        window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    
+        return () => {
+          window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+        };
+      }, []);
+    
+
+    // window.addEventListener("beforeinstallprompt", (e) => {
+    //     console.log("beforeinstallprompt fired");
+    //     e.preventDefault()
+    //     console.log(e)
+        
+    // })
+
+    const install = () => {
+        if (installPrompt) {
+            installPrompt.prompt()
+            installPrompt.userChoice.then((choiceResult : any) => {
+                if (choiceResult.outcome === "accepted") {
+                    console.log("User accepted the install prompt")
+                    setInstallable(false)
+                } else {
+                    console.log("User dismissed the install prompt")
+                }
+            })
+        }
+    }
+
 	return (
-		<Box className="GeoLocalisation">
-            <Button variant="contained" startIcon={<DownloadingIcon />}>
+		<Box className="">
+            <Button variant="contained" startIcon={<DownloadingIcon />} onClick={install} disabled={!installable}>
                 Installer l'application
             </Button>
 		</Box>
