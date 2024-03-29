148bc51cb8883f4778c2d13ee8ec9789c11fe061
Leo-Teixeira <teixeiraleo72@gmail.com>
2024-03-29 13:52:17 +0100

    up

--- src/components/camera/camera.tsx
+++ src/components/camera/camera.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/base";
+import { Button } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState, useEffect, useRef } from "react";
@@ -11,6 +11,8 @@ const Camera = () => {
     Map<string, { photo: string; online: boolean | null }>
   >(new Map());
   const videoRef = useRef(null);
+  const notification = document.querySelector("#notification");
+  const sendButton = document.querySelector("#send");
 
   useEffect(() => {
     localStorage.setItem("listPhoto", JSON.stringify(photos));
@@ -40,6 +42,35 @@ const Camera = () => {
     };
   }, []);
 
+  const sendNotification = async () => {
+    const registration = await navigator.serviceWorker.getRegistration();
+    if (Notification.permission === "granted") {
+      showNotification(notification.value);
+    } else {
+      if (Notification.permission !== "denied") {
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+          showNotification(notification.value);
+        }
+      }
+    }
+  };
+
+  const showNotification = (body) => {
+    const title = "What PWA Can Do Today";
+
+    const payload = {
+      body,
+    };
+
+    if ("showNotification" in registration) {
+      registration.showNotification(title, payload);
+    } else {
+      new Notification(title, payload);
+    }
+  };
+
   const startCamera = async () => {
     if (camera == false) {
       setCamera(true);
@@ -61,6 +92,9 @@ const Camera = () => {
 
   const takePhoto = () => {
     if (videoRef.current) {
+      if (online) {
+        sendButton.addEventListener("click", sendNotification);
+      }
       const canvas = document.createElement("canvas");
       canvas.width = videoRef.current.videoWidth;
       canvas.height = videoRef.current.videoHeight;
