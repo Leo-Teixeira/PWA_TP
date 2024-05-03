bb1fae198dc179b95affd4b005ace51f1e9413d5
PierrePineau <pierrepineau.pro@gmail.com>
2024-05-03 14:53:19 +0200

    SW

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfigFunction = async (phase) => {
 	if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
 		const withPWA = (await import("@ducanh2912/next-pwa")).default({
 			dest: "public",
+            reloadOnOnline: false,
 		})
 		return withPWA(nextConfig)
 	}
