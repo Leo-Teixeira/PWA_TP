fa402fd63ada9f1e48a40eda1da5a43986ab0756
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2024-05-06 16:27:25 +0200

    up

--- src/app/tp1/page.tsx
+++ src/app/tp1/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { Typography, Stack } from "@mui/material";
-import GeoLocalisation from "@/components/GeoLocalisation/GeoLocalisation";
-import Camera from "@/components/camera/camera";
-import DownloadPwa from "@/components/downloadPWA/downloadPWA";
-import IsOnline from "@/components/Buttons/IsOnline";
+import GeoLocalisation from "../../components/GeoLocalisation/GeoLocalisation";
+import Camera from "../../components/camera/camera";
+import DownloadPwa from "../../components/downloadPWA/downloadPWA";
+import IsOnline from "../../components/Buttons/IsOnline";
 
 const Tp1 = () => {
   return (
     <Stack spacing={4} direction="column">
-        <Camera />
-        {/* <GeoLocalisation /> */}
-        {/* <DownloadPwa /> */}
+      <Camera />
+      {/* <GeoLocalisation /> */}
+      {/* <DownloadPwa /> */}
     </Stack>
   );
 };
