be3de96466ebd28028dde02a8814d11b47fde5e4
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2024-05-21 23:04:25 +0200

    test console.log

--- src/app/webotp/page.tsx
+++ src/app/webotp/page.tsx
@@ -1,36 +1,29 @@
 'use client'
 
-import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-
-type CredentialRequestOptions = {
-  otp: OTPOptions;
-  signal: AbortSignal;
-};
-
-type OTPOptions = {
-  transport: string[];
-};
 
 const WebOTP = () => {
   const [otp, setOtp] = useState('');
   const [otpError, setOtpError] = useState('');
 
   useEffect(() => {
-    debugger;
     if ('OTPCredential' in window) {
+        console.log('bien dans window');
       const ac = new AbortController();
-      const otpOption: CredentialRequestOptions = {
+      const otpOption = {
         otp: { transport: ['sms'] },
         signal: ac.signal,
       };
+
       navigator.credentials.get(otpOption).then((otpCredential) => {
-        if (otpCredential) {
+        console.log('credantial trouvée');
+        if (otpCredential && 'code' in otpCredential) {
           console.log('OTP read:', otpCredential);
-          // Use otpCredential.code to get the OTP value
           setOtp((otpCredential as any).code);
         }
       }).catch((err) => {
+        console.error('Failed to auto-read OTP:', err);
         setOtpError('Failed to auto-read OTP: ' + err.message);
       });
 
@@ -52,8 +45,8 @@ const WebOTP = () => {
         height="100vh"
       >
         <Box>
-          {otpError ? (
-            <Typography>{otpError}</Typography>
+          { otpError ? (
+            <Typography color="error">{otpError}</Typography>
           ) : (
             <Typography>My OTP code is: {otp}</Typography>
           )}
