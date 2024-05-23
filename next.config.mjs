871d08c238ae88a59d2ce58fe55b77b791dca445
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2024-05-23 22:46:34 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,24 @@
+
 import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from 'next/constants.js';
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['@sw.js'] = require.resolve('./public/sw.js');
+    }
+    return config;
+  },
 };
 
 const nextConfigFunction = async (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     const withPWA = (await import('@ducanh2912/next-pwa')).default({
-      customWorkerSrc: "worker",
       dest: 'public',
       register: true,
       skipWaiting: true,
+      customWorkerSrc: "worker",
       runtimeCaching: [
         {
           urlPattern: /^https?:\/\/.*\/socket\.io\//,
