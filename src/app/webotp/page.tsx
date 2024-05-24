de3f3b891ea6679ebbc7606d6003ffcd1084cbcc
PierrePineau <pierrepineau.pro@gmail.com>
2024-05-24 10:54:06 +0200

    up

--- src/app/webotp/page.tsx
+++ src/app/webotp/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import WebOTP from "../../components/global/WebOtp";
 import { TextField } from '@mui/material';
 
